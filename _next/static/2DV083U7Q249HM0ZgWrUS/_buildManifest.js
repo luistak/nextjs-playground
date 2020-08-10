5d9df23737e8553e89484dfa32a8ece882a869f9
Luis Takahashi <luis.takahashi@ifood.com.br>
2020-08-10 19:24:39 -0300

    Updates

---
+++ _next/static/2DV083U7Q249HM0ZgWrUS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002Fc9705fa55625e0f81622ebf00bd993aff8f6f1fa.76bd54de74e7d7873b3f.js","static\u002Fcss\u002Fa7c5858728c2d296b932.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
